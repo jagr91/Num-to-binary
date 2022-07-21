a6f2c219b73bd50a26de5ca13bc7ee84ba952ce7
Grzegorz Jastrzebski <gjastrzebski91@gmail.com>
2022-07-21 21:58:28 +0200

    Deleting try/except

--- Main.py
+++ Main.py
@@ -2,23 +2,19 @@ def num_to_bin(number):
     n = 0
     bin_list = []
 
-    try:
-        while 2 ** n <= number:
-            bin_list.insert(0, 2 ** n)
-            n += 1
+    while 2 ** n <= number:
+        bin_list.insert(0, 2 ** n)
+        n += 1
 
-        binary_number = f'Binary of the number {number} is '
+    binary_number = f'Binary of the number {number} is '
 
-        for n in bin_list:
-            if number >= n:
-                number -= n
-                binary_number += '1'
-            else:
-                binary_number += '0'
+    for n in bin_list:
+        if number >= n:
+            number -= n
+            binary_number += '1'
+        else:
+            binary_number += '0'
 
-        print(binary_number)
-    except SyntaxError:
-        print('Wrong input. You need to put an integer')
-
+    print(binary_number)
 
 num_to_bin(1000)
